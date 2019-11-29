2122077d7330ff0cbab0b4ad2a4ca823bf9db7ee
robertidybobito <robertidybobito.berquiste@gmail.com>
2019-11-28 22:16:24 -0600

    implemented startVid and playPause buttons

--- tests/testsjavascript.js
+++ tests/testsjavascript.js
@@ -1,1 +1,14 @@
+function startVid() {
+	var musicplayer = document.getElementById("musicplayer");
+	musicplayer.src = "testvideo.mp4";
+	musicplayer.play();	
+}
 
+function playPause() {
+	var musicplayer = document.getElementById("musicplayer");
+	if (musicplayer.paused) {
+		musicplayer.play();
+	} else {
+		musicplayer.pause();
+	}
+}
