f8dba775c1b8cbd7f664ec3ff99b253b104d3836
robertidybobito <robertidybobito.berquiste@gmail.com>
2019-11-29 10:15:52 -0600

    trying autoplay and auto video change

--- tests/testsjavascript.js
+++ tests/testsjavascript.js
@@ -1,20 +1,26 @@
 var musicplayer;
+var showviddetails;
 
 function onLoad() {
 	musicplayer = document.getElementById("musicplayer");
 	musicplayer.src = "20191128_210922.mp4";
+	showviddetails = document.getElementById("showviddetails");
+	showviddetails.innerHTML = srcParse(musicplayer.src);
+	musicplayer.autoplay = true;
+	musicplayer.addEventListener("ended", changeVid);
 }
 
 function changeVid() {
 	var compare = srcParse(musicplayer.src);
 	if (compare.localeCompare("20191128_210922.mp4") == 0) {
 		alert("changing to video 2");
-		musicplayer.src == "20191128_221149.mp4";
+		musicplayer.src = "20191128_221149.mp4";
 	} else {
 		alert("changing to video 1");
-		musicplayer.src == "20191128_210922.mp4";
+		musicplayer.src = "20191128_210922.mp4";
 	}
 	musicplayer.load();	
+	showviddetails.innerHTML = compare;
 }
 
 function playPause() {
@@ -30,12 +36,16 @@ function srcParse(videosrc) {
 	return res[res.length - 1];
 }
 
+
+
+
 /* ---------------- References ---------------- 
  
 HTML Audio/Video DOM Reference
 https://www.w3schools.com/tags/ref_av_dom.asp
  
- 
+Use event listener to see when video ends
+https://www.w3schools.com/js/js_htmldom_eventlistener.asp
  
  */
 
