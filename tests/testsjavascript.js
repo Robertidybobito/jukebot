ea30194470556e3e3c213d44e59850f649da9560
robertidybobito <robertidybobito.berquiste@gmail.com>
2019-11-28 23:03:57 -0600

    working on changing the currently loaded video

--- tests/testsjavascript.js
+++ tests/testsjavascript.js
@@ -1,14 +1,52 @@
-function startVid() {
-	var musicplayer = document.getElementById("musicplayer");
-	musicplayer.src = "testvideo.mp4";
-	musicplayer.play();	
+var musicplayer;
+
+function onLoad() {
+	musicplayer = document.getElementById("musicplayer");
+	musicplayer.src = "20191128_210922.mp4";
+}
+
+function changeVid() {
+	var compare = srcParse(musicplayer.src);
+	if (compare.localeCompare("20191128_210922.mp4") == 0) {
+		alert("changing to video 2");
+		musicplayer.src == "20191128_221149.mp4";
+	} else {
+		alert("changing to video 1");
+		musicplayer.src == "20191128_210922.mp4";
+	}
+	musicplayer.load();	
 }
 
 function playPause() {
-	var musicplayer = document.getElementById("musicplayer");
 	if (musicplayer.paused) {
 		musicplayer.play();
 	} else {
 		musicplayer.pause();
 	}
 }
+
+function srcParse(videosrc) {
+	var res = videosrc.split("/");
+	return res[res.length - 1];
+}
+
+/* ---------------- References ---------------- 
+ 
+HTML Audio/Video DOM Reference
+https://www.w3schools.com/tags/ref_av_dom.asp
+ 
+ 
+ 
+ */
+
+
+/* ---------------- Unused ----------------
+
+function startVid() {
+	var musicplayer = document.getElementById("musicplayer");
+	musicplayer.src = "testvideo.mp4";
+	musicplayer.play();	
+}
+
+
+*/
